b7957d43d352f0cfe7445b9b30408c0041f778e0
Bailey Miller <bmiller@susquehannasoftware.com>
2017-08-16 16:05:28 -0400

    Some basic math operations.

--- CalculatorApi/CalculatorApi/Calculator.cs
+++ CalculatorApi/CalculatorApi/Calculator.cs
@@ -8,5 +8,37 @@ namespace CalculatorApi
 {
     public class Calculator
     {
+        public Calculator()
+        {
+
+        }
+
+        public int Sum(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+        public double Sum(params double[] numbers)
+        {
+            return numbers.Sum();
+        }
+
+        public int Subtract(params int[] numbers)
+        {
+            var total = 0;
+            foreach (var number in numbers)
+            {
+                total -= number;
+            }
+            return total;
+        }
+        public double Subtract(params double[] numbers)
+        {
+            var total = 0.0;
+            foreach (var number in numbers)
+            {
+                total -= number;
+            }
+            return total;
+        }
     }
 }
