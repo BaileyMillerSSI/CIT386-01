40b6f9c515237f6d2fae55d826c1891caca95605
Bailey Miller <bmiller@susquehannasoftware.com>
2017-08-18 11:58:50 -0400

    We got a bug batman. But data binding works like Wpf does with slight changes with the new x:binding. The text block doesn't scale correctly.

--- CalculatorUI (Uwp)/CalculatorUI (Uwp)/MainPage.xaml.cs
+++ CalculatorUI (Uwp)/CalculatorUI (Uwp)/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -20,16 +21,35 @@ namespace CalculatorUI__Uwp_
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        private string _DisplayText { get; set; } = "|";
+
+        public string DisplayText
+        {
+            get {
+                return _DisplayText;
+            } set {
+                _DisplayText = value;
+                RaisePropertyChanged("DisplayText");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
         private void OnCalculatorBtnClick(object sender, RoutedEventArgs e)
         {
-
+            DisplayText += (sender as Button).Content as string;
         }
     }
 }
