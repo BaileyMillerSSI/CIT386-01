a828ee271af29762751e9f4a17f9f099c3e32d4c
Bailey Miller <bmiller@susquehannasoftware.com>
2017-08-22 11:02:51 -0400

    Finished the calculator in WPF.

---
+++ CalculatorApi (NetStandard)/CalculatorApi (NetStandard)/CalculatorViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApi
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event handler required for data binding to update on the view when information changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Method required for executing the changed event
+        /// Updated to use a special C# feature that allows for the property name to be taken from the code rather than hard coded in
+        /// </summary>
+        /// <param name="name">The name of the property to be updated for data-binding</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private StringBuilder _DisplayText { get; set; } = new StringBuilder("|");
+
+        public String DisplayText
+        {
+            get
+            {
+                return _DisplayText.ToString();
+            }set
+            {
+                if (TextIsWaitCursor())
+                {
+                    _DisplayText = new StringBuilder();
+                }
+                _DisplayText.Append(value);
+                OnPropertyChanged();
+            }
+        }
+        
+
+        /// <summary>
+        /// Removes all spaces from the incoming problem statement, etc 1 +2 is 1+2
+        /// </summary>
+        /// <returns>The problem statement clear of spaces</returns>
+        public async Task<String> ClearProblemOfSpaces()
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                var problem = DisplayText;
+
+                var problemCharArray = problem.ToCharArray();
+                var cleanProblem = new StringBuilder();
+                foreach (var nonSpaces in problemCharArray.Where(x => x != ' ').Where(x=> x != '|'))
+                {
+                    cleanProblem.Append(nonSpaces);
+                }
+
+                return cleanProblem.ToString();
+
+            });
+        }
+
+        public void Clear()
+        {
+            _DisplayText = new StringBuilder("|");
+        }
+
+        public void Backup()
+        {
+            var CurrentCount = _DisplayText.Length;
+            _DisplayText = new StringBuilder(_DisplayText.ToString().Substring(0, CurrentCount - 1));
+            OnPropertyChanged("DisplayText");
+        }
+
+        public bool TextIsWaitCursor()
+        {
+            return _DisplayText.ToString().Trim() == "|";
+        }
+
+        public void SetAnswer(object input)
+        {
+            _DisplayText = new StringBuilder(input.ToString());
+
+            OnPropertyChanged("DisplayText");
+        }
+
+
+    }
+}
--- CalculatorUI/CalculatorUI/MainWindow.xaml.cs
+++ CalculatorUI/CalculatorUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CalculatorApi;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,48 +21,26 @@ namespace CalculatorUI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
+
+        CalculatorViewModel vm = new CalculatorViewModel();
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = vm;
         }
 
-        private string _DisplayText { get; set; } = "|";
-
-        public String DisplayText
+        public async void OnCalculatorBtnClick(object sender, RoutedEventArgs e)
         {
-            get
-            {
-                return _DisplayText;
-            }
-            private set
-            {
-                _DisplayText = value;
-                OnPropertyChanged("DisplayText");
-            }
-        }
-        
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            //Do the same thing as a key press for that button
+            
         }
 
-        public void OnCalculatorBtnClick(object sender, RoutedEventArgs e)
-        {
-
-            if (TextIsWaitCursor())
-            {
-                ClearTextArea();
-            }
-
-            DisplayText += (sender as Button).Content as string; 
-        }
-
-        private void ListenForKeyPresses(object sender, KeyEventArgs e)
+        private async void ListenForKeyPresses(object sender, KeyEventArgs e)
         {
             //To-Do listen for any number key up events
             // Any symbol up events, +, -, /, *, (=, Enter)
@@ -68,51 +48,70 @@ namespace CalculatorUI
             switch (e.Key)
             {
                 case Key.Add:
-                    DisplayText += "+";
+                    vm.DisplayText = "+";
                     break;
                 case Key.Subtract:
-                    DisplayText += "-";
+                    vm.DisplayText = "-";
                     break;
                 case Key.Multiply:
-                    DisplayText += "*";
+                    vm.DisplayText = "*";
                     break;
                 case Key.Divide:
-                    DisplayText += "/";
+                    vm.DisplayText = "/";
                     break;
-                case Key.OemPlus | Key.Enter:
-                    //This is the equal key
+                case Key.OemPlus:
+                    var _answer1 = await SolveProblemAsync();
+                    vm.SetAnswer(_answer1);
+                    break;
+                case Key.Enter:
+                    var answer = await SolveProblemAsync();
+                    vm.SetAnswer(answer);
                     break;
 
                 //Now for the numbers
                 case Key.NumPad1:
+                    vm.DisplayText = "1";
                     break;
                 case Key.NumPad2:
+                    vm.DisplayText = "2";
                     break;
                 case Key.NumPad3:
+                    vm.DisplayText = "3";
                     break;
                 case Key.NumPad4:
+                    vm.DisplayText = "4";
                     break;
                 case Key.NumPad5:
+                    vm.DisplayText = "5";
                     break;
                 case Key.NumPad6:
+                    vm.DisplayText = "6";
                     break;
                 case Key.NumPad7:
+                    vm.DisplayText = "7";
                     break;
                 case Key.NumPad8:
+                    vm.DisplayText = "8";
                     break;
                 case Key.NumPad9:
+                    vm.DisplayText = "9";
                     break;
                 case Key.NumPad0:
+                    vm.DisplayText = "0";
                     break;
 
                 //Functional helpers
                 //Clear simple backspace
                 case Key.Escape:
                     //This should clear the text
-                    DisplayText = String.Empty;
+                    vm.Clear();
                     break;
-                case Key.Delete | Key.Back:
-                    //Remove the last entered character
+                case Key.Delete:
+                    vm.Backup();
+                    break;
+
+                case Key.Back:
+                    vm.Backup();
                     break;
 
                 default:
@@ -121,19 +120,23 @@ namespace CalculatorUI
 
         }
 
-        private void NotifyTextChange()
-        {
-            OnPropertyChanged("DisplayText");
-        }
 
-        private Boolean TextIsWaitCursor()
+        public async Task<double> SolveProblemAsync()
         {
-            return DisplayText == "|";
-        }
-
-        private void ClearTextArea()
-        {
-            DisplayText = String.Empty;
+            return await await Task.Factory.StartNew(async () =>
+            {
+                var clean = await vm.ClearProblemOfSpaces();
+                var solver = new DataTable();
+                var didSolve = Double.TryParse(solver.Compute(clean, "").ToString(), out double sol);
+                if (didSolve)
+                {
+                    return sol;
+                }
+                else
+                {
+                    return 0.0;
+                }
+            });
         }
     }
 }
