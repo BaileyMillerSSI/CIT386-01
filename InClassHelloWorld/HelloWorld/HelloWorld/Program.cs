bf1b399770a083ff71f79483f5f5271a13964926
Bailey Miller <bmiller@susquehannasoftware.com>
2017-08-22 11:17:45 -0400

    Finished it

--- InClassHelloWorld/HelloWorld/HelloWorld/Program.cs
+++ InClassHelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -10,6 +10,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.Title = "I am ALIVE!";
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
 
         }
     }
